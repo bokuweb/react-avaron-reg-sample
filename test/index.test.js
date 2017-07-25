783640439d24afc35122e1a352f1e5889aefd680
bokuweb <bokuweb12@gmail.com>
2017-07-25 16:06:20 +0900

    modify

--- test/index.test.js
+++ test/index.test.js
@@ -15,7 +15,7 @@ const Example = () => (
     <MuiThemeProvider>
         <DatePickerDialog
             ref={(e) => { datePicker = e; }}
-            initialDate={new Date(2017, 6, 1)}
+            initialDate={new Date(2017, 9, 1)}
             firstDayOfWeek={0}
             mode="landscape"
         />
